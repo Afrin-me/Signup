d6ec6bf0cb71394142d5a84d8c63e15883b5e0ae
Afrin-me <shaikhafrin1125@gmail.com>
2024-12-08 21:40:56 +0530

    signup code

---
+++ index.js
@@ -0,0 +1,13 @@
+function userName(signup){
+    let userArr = ["afrin","alice","bob","char"];
+
+    if (usersArr.includes(userName)) {
+      return "User Already Registered, Please Login";
+    } else {
+      // Add new username to the array
+      usersArr.push(userName);
+      return "Signup Successful, Please Login";
+    }
+}
+console.log(registerUser("alice"));
+console.log(registerUser("diamond"));
